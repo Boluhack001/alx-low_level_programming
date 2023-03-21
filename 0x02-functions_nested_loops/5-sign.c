ef87453f8b733313f78667487a6d50fd63339464
idrees <moboladeidrees@gmail.com>
2023-03-21 15:09:51 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: An input number
+ *
+ * Return: 1 if number is poditive, 0 if number is 0
+ * and -1 if number is negstive
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n < 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
