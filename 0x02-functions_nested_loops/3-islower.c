2f531d95f12af60097c553c759da2679f4e2181c
idrees <moboladeidrees@gmail.com>
2023-03-21 11:56:29 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * int _islower - To determine a lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{return (1);
+	else
+		return (0);
+	}
+}
