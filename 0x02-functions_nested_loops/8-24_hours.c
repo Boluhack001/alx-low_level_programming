74b8b77dfeafd6f5113c18699aee3a41e59cf815
idrees <moboladeidrees@gmail.com>
2023-03-22 15:49:23 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Description: Functions print every minute in 1 day
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int Hour;
+	int Minute;
+
+	for (Hour = 0; Hour < 24; Hour++)
+	{
+		for (Minute = 0; Minute < 60; Minute++)
+		{
+			_putchar((Hour / 10) + '0');
+			_putchar((Hour % 10) + '0');
+			_putchar(':');
+			_putchar((Minute / 10) + '0');
+			_putchar((Minute / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
