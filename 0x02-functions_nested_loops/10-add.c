7daffe21e8bdea2f94c847d71809756f82dbea23
idrees <moboladeidrees@gmail.com>
2023-03-22 21:18:33 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: storage input
+ * @b: storage input
+ * Description: Function that add 2 integers and return the value
+ * Return: a and b
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
