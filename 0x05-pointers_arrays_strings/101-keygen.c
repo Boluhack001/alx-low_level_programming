f923ed3c3ca0f8a21b30e98aeebb8dca7555958d
idrees <moboladeidrees@gmail.com>
2023-03-28 21:29:40 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - programme that generate random valid password
+ * @void: Empty input
+ *
+ * Return: 0 if works
+ */
+int main(void)
+{
+	int = 0, random, checksum;
+
+	srand(time(NULL));
+	checksum = 2772;
+	while (checksum > 122)
+	{
+		random = (rand() % 100);
+		printf("%c", random);
+		checksum -= random;
+		i++;
+	}
+	printf("%c", checksum);
+	return (0);
+}
