2e41b8ee20e0a36754125b435357dda08f3052cb
idrees <moboladeidrees@gmail.com>
2023-04-03 21:44:35 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - Get the length of a prefix substring
+ * @s: inpyt string to search for substring
+ * @accept: character that prefix substring must includd
+ *
+ * Return: length of a prefix substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, a_len = 0, len = 0;
+
+	 while (accept[a_len] != '\0')
+		a_len++;
+	for (i = 0; s[i] != '\0'; i++) 
+		for (j = 0; j < a_len; j++)
+			if (s[i] == acceptj[j])
+				len++, j = a_len;
+			else
+				if (j == a_len - 1)
+					goto exit;
+exit: return (len);
+}
